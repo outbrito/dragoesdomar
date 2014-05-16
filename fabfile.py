02ddfe6c67e8d2ac885c5fa6057e1137bb762f42
Thiago Paiva <tpborion@gmail.com>
2014-05-16 00:19:15 -0300

    Adicionando o fabfile para facilitar deploy

--- fabfile.py
+++ fabfile.py
@@ -2,27 +2,20 @@ from __future__ import with_statement
 from fabric.api import *
 
 env.hosts = [
-    'soda@novocasatudo.sodateste.com.br',
+    'ubuntu@150.165.15.94:9022',
 ]
 env.warn_only = True
 
 def deploy():
-    with cd('/deploy/sites/novocasatudo/novocasatudo'):
+    with cd('/opt/dragoesdomar'):
         # get lastest version from git
         run('git pull')
-        run('../bin/pip install -r requirements.txt')
-        run('../bin/python manage.py rebuild_index --settings=src.settings_production')
-        # restart nginx
-        run('supervisorctl restart novocasatudo')
+        run('../venvs/dragoesdomar/bin/pip install -r requirements.txt')
+
+        # restart
+        run('killall python')
+        run('../venvs/dragoesdomar/bin/python manage_production.py runfcgi host=127.0.0.1 port=8080')
+        run('sudo service nginx restart')
 
 
-def deploy_migrate():
-    with cd('/deploy/sites/novocasatudo/novocasatudo'):
-        # get lastest version from git
-        run('git pull')
-        run('../bin/pip install -r requirements.txt')
-        run('../bin/python manage.py syncdb --settings=src.settings_production')
-        run('../bin/python manage.py migrate --settings=src.settings_production')
-        run('../bin/python manage.py rebuild_index --settings=src.settings_production')
-        # restart nginx
-        run('supervisorctl restart novocasatudo')
+#def deploy_migrate():
